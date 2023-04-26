13aa23933a8ddaddcbec2670d84e823d1b7a67ac
Jingnan Shi <jnshi@mit.edu>
2023-04-25 23:34:09 -0400

    docker file wip

---
+++ docker/start_docker.sh
@@ -0,0 +1,12 @@
+docker run -it --user="$(id -u $USER)":"$(id -g $USER)" \
+--gpus all \
+--shm-size 8G \
+--env="DISPLAY=:1" \
+--env="CUDA_VISIBLE_DEVICES=0" \
+--volume="/home/jnshi/code/gdrnpp_bop2022/:/opt/project/gdrnpp_bop2022" \
+--volume="/mnt/jnshi_data/datasets/casper-data:/mnt/datasets/" \
+--volume="/etc/group:/etc/group:ro" \
+--volume="/etc/passwd:/etc/passwd:ro" \
+--volume="/etc/shadow:/etc/shadow:ro" \
+--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" gdrnet:latest bash
